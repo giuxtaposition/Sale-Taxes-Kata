8906c96f6df5e42ad5ff8b24de592b1a514e176a
giuxtaposition <yg97.cs@gmail.com>
2021-07-26 10:16:14 +0200

    added function to format name to standard

--- libs/classes/Product.ts
+++ libs/classes/Product.ts
@@ -9,12 +9,26 @@ export default class Product {
   taxApplied: number
 
   constructor(product: product) {
-    this.name = product.name
+    this.name = this.formatName(product.name)
     this.quantity = product.quantity
     this.price = product.price
     this.imported = product.imported
     this.category = product.category
     this.taxedPrice = product.taxedPrice
     this.taxApplied = product.taxApplied
+  }
+
+  formatName(name: string): string {
+    if (name.includes('imported')) {
+      const match = /^imported\W/.exec(name)
+      if (!match) {
+        const formattedName = 'imported '.concat(name.replace('imported ', ''))
+        return formattedName
+      } else {
+        return name
+      }
+    } else {
+      return name
+    }
   }
 }
